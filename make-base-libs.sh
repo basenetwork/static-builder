7b6060802aa6c702d3664aa81bfe8710f01b9466
Denis Glazkov <denis.glazkov@lazada.com>
2015-07-31 16:14:13 +0300

    add external nodes

--- make-base-libs.sh
+++ make-base-libs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # core client lib
-./make.js ../client-js/lib/core.js            >../client-js/core-1.0.js
+#./make.js ../client-js/lib/core.js            >../client-js/core-1.0.js
+./make.js ../client-js/lib/core.js            >../node-server/data/core.js
 
 # site app std
 ./make.js ../site-engine-js/app-std/main.css  >../site-engine-js/app-std-1.0.css
